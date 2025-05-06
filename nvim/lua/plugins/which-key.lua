43961ddedf95305ba2c89a364b7fee2e81661881
Dorian Raboy Mcgowan <dorianr@zillowgroup.com>
2025-05-06 15:51:49 -0700

    chore: update which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,5 +1,23 @@
 return {
-	{ "folke/which-key.nvim", event = "VeryLazy", opts = {} },
-	-- Use :checkhealth which-key to display conflicting keymaps
-	-- z= on word for spelling suggestions, ' for marks, " for registers
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	}
 }
+
+-- Use :checkhealth which-key to display conflicting keymaps
+-- z= on word for spelling suggestions, ' for marks, " for registers
