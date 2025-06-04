9e1af036b96a2f43d4b07c0796a36933a10abf13
Dorian Raboy Mcgowan <dorianr@zillowgroup.com>
2025-06-04 16:22:32 -0700

    update: diagnostics

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -41,19 +41,51 @@ vim.o.laststatus = 3
 
 -- Highlight when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.hl.on_yank()
-    end,
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
+
+-- vim.diagnostic.config({
+--     severity_sort = true,
+--     virtual_text = true,
+--     virtual_lines = {
+--         current_line = true,
+--     }
+-- })
 
 vim.diagnostic.config({
-    severity_sort = true,
-    virtual_text = true,
-    virtual_lines = {
-        current_line = true,
-    }
+	virtual_lines = true,
+	virtual_text = {
+		source = "if_many",
+		spacing = 2,
+		format = function(diagnostic)
+			local diagnostic_message = {
+				[vim.diagnostic.severity.ERROR] = diagnostic.message,
+				[vim.diagnostic.severity.WARN] = diagnostic.message,
+				[vim.diagnostic.severity.INFO] = diagnostic.message,
+				[vim.diagnostic.severity.HINT] = diagnostic.message,
+			}
+			return diagnostic_message[diagnostic.severity]
+		end,
+	},
+	float = { border = "rounded", source = "if_many" },
+	underline = { severity = vim.diagnostic.severity.ERROR },
+	update_in_insert = false,
+	severity_sort = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
 })
-
 
 -- -- Autocomplete
 -- vim.api.nvim_create_autocmd('LspAttach', {
