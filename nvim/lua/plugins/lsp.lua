2a0e0bec78d05077820c013099c26804e96e746a
Dorian Raboy Mcgowan <dorianr@zillowgroup.com>
2025-06-06 10:51:10 -0700

    chore: cleanups

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,18 +1,5 @@
 return {
 	{
     "neovim/nvim-lspconfig",
-
-    -- config = function()
-    -- vim.api.nvim_create_autocmd('LspAttach', {
-    --   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-    --   callback = function(ev)
-    --     local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    --     if client:supports_method('textDocument/completion') then
-    --       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    --     end
-    --   end,
-    -- })
-    --
-    -- end
   }
 }
