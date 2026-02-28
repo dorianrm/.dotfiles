12af0d932a57feb9d2ef6a49639c13936c0f9f5d
Dorian Raboy Mcgowan <dorianr@zillowgroup.com>
2026-02-27 17:19:40 -0800

    feat: add markdown formatting and markdownlint config
    - Add markdown formatter (prettierd/prettier) with prose-wrap
    - Configure black line-length to 79
    - Add markdownlint with custom config path
    - Add markdownlint dotfile

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -11,12 +11,34 @@ return {
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				go = { "gofumpt" },
+				markdown = { "prettierd", "prettier", stop_after_first = true },
 			},
 			-- format_on_save = {
 			-- 	-- These options will be passed to conform.format()
-			-- 	timeout_ms = 500,
+			-- 	timeout_ms = 2000,
 			-- 	lsp_format = "fallback",
 			-- },
+			formatters = {
+				black = {
+					prepend_args = { "--line-length", "79" },
+				},
+				prettierd = {
+					prepend_args = function(_, ctx)
+						if vim.bo[ctx.buf].filetype == "markdown" then
+							return { "--prose-wrap", "always", "--print-width", "120" }
+						end
+						return {}
+					end,
+				},
+				prettier = {
+					prepend_args = function(_, ctx)
+						if vim.bo[ctx.buf].filetype == "markdown" then
+							return { "--prose-wrap", "always", "--print-width", "120" }
+						end
+						return {}
+					end,
+				},
+			},
 		},
 	},
 }
