fae84b6421200f971b49abd09988165aae4b2200
Dorian Raboy <dorianr@zillowgroup.com>
2026-08-04 23:15:46 -0700

    refactor: consolidate git plugins, remove dead jdtls config
    - merge vim-fugitive.lua + gitsigns.lua into git.lua, matching Kun's grouped-by-concern pattern
    - remove jdtls.lua: fully commented out, returned an empty spec, superseded by nvim-java.lua

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -8,4 +8,10 @@ return {
 			{ "<leader>gl", "<cmd>Git log --oneline<cr>", desc = "Git Log (Fugitive)" },
 		},
 	},
+	{
+		-- Adds git releated signs to the gutter & utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		event = "BufWinEnter",
+		opts = {},
+	},
 }
